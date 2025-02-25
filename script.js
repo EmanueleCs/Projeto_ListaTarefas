8822354e7156eacc8440897f6dd95a5edbe7ecd3
EmanueleCs <127628645+EmanueleCs@users.noreply.github.com>
2025-02-25 11:05:49 -0300

    Funções
    removerTarefa, editarTarefa e limparLista. Botão LimparLista só aparece se não existir e é removido quando a lista é limpa.

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ function adicionarTarefa() {
 
         tarefas.push(tarefa)
         renderizarTarefas()
+
+        //verifica se existe elemento com a classe botao_lista, caso não tenh, é criado um botão
+        if (!document.querySelector(".botao_lista")) {
+            let botaoLimpaLista = document.createElement("button")
+            botaoLimpaLista.className = "botao_lista"
+            botaoLimpaLista.textContent = "Limpar tudo"
+            botaoLimpaLista.onclick = () => limparLista()
+
+            //pega a div em que estão os botões e adiciona o botapLimpaTudo 
+            let botoes = document.getElementById("botoes")
+            botoes.appendChild(botaoLimpaLista)
+        }
     }
 
     inputTarefa.value = ""
@@ -33,17 +45,56 @@ function renderizarTarefas() {
             //selecionado a ul e criando uma nova li com createElement
             let novaTarefa = document.createElement("li")
             novaTarefa.textContent = tarefas[i]
+            
+            //criando botao remover tarefa
+            let botaoRemover = document.createElement("button")
+            botaoRemover.className = "remover"
+            botaoRemover.textContent = "Remover"
+            botaoRemover.onclick = () => removerTarefa(i) //arrow function
+            
+            let botaoEditar = document.createElement("button")
+            botaoEditar.className = "editar"
+            botaoEditar.textContent = "Editar"
+            botaoEditar.onclick = () => editarTarefa(i)
+
+            novaTarefa.appendChild(botaoRemover)
+            novaTarefa.appendChild(botaoEditar)
             //criar um elemento filho
             listaTarefas.appendChild(novaTarefa)
             
         }
 
+        
+
 }
 
-function apagarTarefa() {
-    
+function removerTarefa(i) {
+    tarefas.splice(i, 1) //i = indice e 1 é quantidade
+    renderizarTarefas()
+
 }
 
-function editarTarefa() {
-    
+function editarTarefa(i) { 
+    let tarefaEditada = prompt("Edite a tarefa:")
+
+    if (tarefaEditada.trim() !== "") {
+        tarefas[i] = tarefaEditada
+        renderizarTarefas()
+    }
+
 }
+
+function limparLista(){
+    tarefas.length = 0
+    renderizarTarefas()
+
+    mensagem.textContent = "Lista de tarefas limpa com sucesso!"
+    removerBotaoLimparLista()
+}
+
+function removerBotaoLimparLista() {
+    document.querySelector(".botao_lista").remove()
+}
+
+// ao limpar a lista o botao limpar tudo deve sumir 
+// ele está sendo adicionado diversas vezes
